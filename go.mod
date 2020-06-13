d5cdb5c85277b324da4260fb4245b673f09ecae1
Tomas Doran <tdoran@yelp.com>
2020-06-13 14:19:48 +0100

    modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bobtfish/mayhem
+
+go 1.14
+
+require github.com/faiface/pixel v0.9.0 // indirect
--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+package main
+
 import "fmt"
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+)
 
 func main() { fmt.Println("hello world") }
