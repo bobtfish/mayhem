6de955344ced85362d555dc80003ae0ccec490eb
Tomas Doran <tdoran@yelp.com>
2020-06-14 00:25:59 +0100

    More colours

--- character.go
+++ character.go
@@ -58,15 +58,15 @@ func (c *Character) GetSprite(ss pixel.Picture) *pixel.Sprite {
 	spriteLocation := c.Sprites[c.SpriteIdx]
 	x := spriteLocation[0]
 	y := spriteLocation[1]
-	fmt.Printf("Character %s has %d sprites, in sprite sheet sprite 0 is x %d, y %d topx %d topy %d\n", c.Name, len(c.Sprites), x*SPRITE_SIZE, y*SPRITE_SIZE, x*SPRITE_SIZE+SPRITE_SIZE, y*SPRITE_SIZE+SPRITE_SIZE)
+//	fmt.Printf("Character %s has %d sprites, in sprite sheet sprite 0 is x %d, y %d topx %d topy %d\n", c.Name, len(c.Sprites), x*SPRITE_SIZE, y*SPRITE_SIZE, x*SPRITE_SIZE+SPRITE_SIZE, y*SPRITE_SIZE+SPRITE_SIZE)
 	return pixel.NewSprite(ss, pixel.R(float64(x*SPRITE_SIZE), float64(y*SPRITE_SIZE), float64(x*SPRITE_SIZE+SPRITE_SIZE), float64(y*SPRITE_SIZE+SPRITE_SIZE)))
 }
 
 func (c *Character) GetColorMask() color.Color {
 	if c.ColorR == 0 && c.ColorG == 0 && c.ColorB == 0 {
-		return pixel.RGB(255, 255, 255)
+		return pixel.RGB(1, 1, 1)
 	}
-	return pixel.RGB(float64(c.ColorR), float64(c.ColorG), float64(c.ColorB))
+	return pixel.RGB(float64(c.ColorR)/255, float64(c.ColorG)/255, float64(c.ColorB)/255)
 }
 
 func (c *Character) GetText(x, y int) *text.Text {
