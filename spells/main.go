37ee9949572eab6c51b775a333b29fc62eace75a
Tomas Doran <tdoran@yelp.com>
2020-06-19 21:19:24 +0100

    More

--- spells/main.go
+++ spells/main.go
@@ -1,15 +1,35 @@
 package spells
 
 type Spell struct {
-	Name string
+	Name      string
+	LawRating int
+}
+
+func (s Spell) LawRatingSymbol() string {
+	if s.LawRating == 0 {
+		return "-"
+	}
+	if s.LawRating < 0 {
+		return "*"
+	}
+	return "^"
 }
 
 var AllSpells []Spell
 
 func init() {
 	AllSpells = []Spell{
-		Spell{Name: "Spell1"},
-		Spell{Name: "Spell2"},
-		Spell{Name: "Spell3"},
+		Spell{
+			Name:      "Spell1",
+			LawRating: 0,
+		},
+		Spell{
+			Name:      "Spell2",
+			LawRating: 1,
+		},
+		Spell{
+			Name:      "Spell3",
+			LawRating: -1,
+		},
 	}
 }
