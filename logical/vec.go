88b9bb26088380f1158b7ee763681bdbc7769667
Tomas Doran <tdoran@yelp.com>
2020-07-03 12:26:12 +0100

    Add, multiply, zero and identity vecs

--- logical/vec.go
+++ logical/vec.go
@@ -5,14 +5,21 @@ type Vec struct {
 	Y int
 }
 
+func ZeroVec() Vec     { return Vec{0, 0} }
+func IdentityVec() Vec { return Vec{1, 1} }
+
 func V(x, y int) Vec {
 	return Vec{X: x, Y: y}
 }
 
 func (v Vec) Add(w Vec) Vec {
-    return Vec{v.X+w.X, v.Y+w.Y}
+	return Vec{v.X + w.X, v.Y + w.Y}
 }
 
 func (v Vec) Subtract(w Vec) Vec {
-    return Vec{v.X-w.X, v.Y-w.Y}
+	return Vec{v.X - w.X, v.Y - w.Y}
+}
+
+func (v Vec) Multiply(w Vec) Vec {
+	return Vec{v.X * w.X, v.Y * w.Y}
 }
--- logical/vec_test.go
+++ logical/vec_test.go
@@ -13,21 +13,61 @@ func TestVec(t *testing.T) {
 }
 
 func TestVecAdd(t *testing.T) {
-    v := V(1, 2).Add(V(1, 2))
-    if v.X != 2 {
-        t.Errorf("v(1, 2) + v(1, 2) x != 2")
-    }
-    if v.Y != 4 {
-        t.Errorf("v(1, 2) + v(1, 2) x != 4")
-    }
+	v := V(1, 2).Add(V(1, 2))
+	if v.X != 2 {
+		t.Errorf("v(1, 2) + v(1, 2) x != 2")
+	}
+	if v.Y != 4 {
+		t.Errorf("v(1, 2) + v(1, 2) y != 4")
+	}
 }
 
 func TestVecSubtract(t *testing.T) {
-    v := V(4, 8).Subtract(V(1, 2))
-    if v.X != 3 {
-        t.Errorf("v(4, 8) - v(1, 2) x != 3 is %d", v.X)
-    }
-    if v.Y != 6 {
-        t.Errorf("v(4, 8) + v(1, 2) x != 6 is %d", v.Y)
-    }
+	v := V(4, 8).Subtract(V(1, 2))
+	if v.X != 3 {
+		t.Errorf("v(4, 8) - v(1, 2) x != 3 is %d", v.X)
+	}
+	if v.Y != 6 {
+		t.Errorf("v(4, 8) + v(1, 2) y != 6 is %d", v.Y)
+	}
+}
+
+func TestVecMultiply(t *testing.T) {
+	v := V(2, 3).Multiply(V(2, 2))
+	if v.X != 4 {
+		t.Errorf("v(2, 3) * v(2, 2) x != 4 is %d", v.X)
+	}
+	if v.Y != 6 {
+		t.Errorf("v(2, 3) * v(2, 2) y != 6 is %d", v.Y)
+	}
+}
+
+func TestVecIdentity(t *testing.T) {
+	v := V(2, 3).Multiply(IdentityVec())
+	if v.X != 2 {
+		t.Errorf("v(2, 3) * IdentityVec() x != 2 is %d", v.X)
+	}
+	if v.Y != 3 {
+		t.Errorf("v(2, 3) * IdentityVec() y != 3 is %d", v.Y)
+	}
+}
+
+func TestVecZeroMultiply(t *testing.T) {
+	v := V(2, 3).Multiply(ZeroVec())
+	if v.X != 0 {
+		t.Errorf("v(2, 3) * ZeroVec() x != 0 is %d", v.X)
+	}
+	if v.Y != 0 {
+		t.Errorf("v(2, 3) * ZeroVec() y != 0 is %d", v.Y)
+	}
+}
+
+func TestVecZeroAdd(t *testing.T) {
+	v := V(2, 3).Add(ZeroVec())
+	if v.X != 2 {
+		t.Errorf("v(2, 3) + ZeroVec() x != 2 is %d", v.X)
+	}
+	if v.Y != 3 {
+		t.Errorf("v(2, 3) + ZeroVec() y != 3 is %d", v.Y)
+	}
 }
