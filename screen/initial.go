289a0372b9d8763b91c391c10aa4f56ce8008beb
Tomas Doran <tdoran@yelp.com>
2020-07-04 16:57:15 +0100

    Cleanup conditional logic

--- screen/initial.go
+++ screen/initial.go
@@ -9,50 +9,45 @@ import (
 	"github.com/bobtfish/mayhem/logical"
 )
 
-type InitialScreen struct {
-	DrawnFirst         bool
-	WizardCount        int
-	DrawnSecond        bool
-	ComputerDifficulty int
+type InitialScreen struct{}
+type ComputerDifficultyScreen struct {
+	WizardCount int
 }
 
 func (screen *InitialScreen) Enter(ss pixel.Picture, win *pixelgl.Window) {
 	ClearScreen(ss, win)
+	td := TextDrawer(ss)
+	td.DrawText("  MAYHEM - Remake of Chaos", logical.V(0, 9), win)
+	td.DrawText("         By bobtfish", logical.V(0, 8), win)
+	td.DrawText("How many wizards?", logical.V(0, 6), win)
+	td.DrawText("(Press 2 to 8)", logical.V(0, 5), win)
 }
 
 func (screen *InitialScreen) Step(ss pixel.Picture, win *pixelgl.Window) GameScreen {
-	td := TextDrawer(ss)
-	if !screen.DrawnFirst {
-		td.DrawText("  MAYHEM - Remake of Chaos", logical.V(0, 9), win)
-		td.DrawText("         By bobtfish", logical.V(0, 8), win)
-		td.DrawText("How many wizards?", logical.V(0, 6), win)
-		td.DrawText("(Press 2 to 8)", logical.V(0, 5), win)
-		screen.DrawnFirst = true
-	} else {
-		if screen.WizardCount == 0 {
-			c := captureNumKey(win)
-			if c >= 2 && c <= 8 {
-				screen.WizardCount = c
-				td.DrawText(fmt.Sprintf("%d", c), logical.V(18, 6), win)
-			}
-		} else {
-			if !screen.DrawnSecond {
-				td.DrawText("Level of computer wizards?", logical.V(0, 3), win)
-				td.DrawText("(Press 1 to 8)", logical.V(0, 2), win)
-				screen.DrawnSecond = true
-			} else {
-				c := captureNumKey(win)
-				if c >= 1 && c <= 8 {
-					screen.ComputerDifficulty = c
-					td.DrawText(fmt.Sprintf("%d", c), logical.V(27, 3), win)
-				}
-			}
-		}
+	c := captureNumKey(win)
+	if c >= 2 && c <= 8 {
+		td := TextDrawer(ss)
+		td.DrawText(fmt.Sprintf("%d", c), logical.V(18, 6), win)
+		td.DrawText("Level of computer wizards?", logical.V(0, 3), win)
+		td.DrawText("(Press 1 to 8)", logical.V(0, 2), win)
+		return &ComputerDifficultyScreen{WizardCount: c}
 	}
-	if screen.ComputerDifficulty > 0 && screen.WizardCount > 0 {
+	return screen
+}
+
+func (screen *ComputerDifficultyScreen) Enter(ss pixel.Picture, win *pixelgl.Window) {
+	td := TextDrawer(ss)
+	td.DrawText("Level of computer wizards?", logical.V(0, 3), win)
+	td.DrawText("(Press 1 to 8)", logical.V(0, 2), win)
+}
+
+func (screen *ComputerDifficultyScreen) Step(ss pixel.Picture, win *pixelgl.Window) GameScreen {
+	c := captureNumKey(win)
+	if c >= 1 && c <= 8 {
+		TextDrawer(ss).DrawText(fmt.Sprintf("%d", c), logical.V(27, 3), win)
 		return &PlayersScreen{
 			WizardCount:        screen.WizardCount,
-			ComputerDifficulty: screen.ComputerDifficulty,
+			ComputerDifficulty: c,
 		}
 	}
 	return screen
