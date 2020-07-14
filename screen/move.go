f040ab535be89ff62a6c4054eb87e15ac1dbd85a
Tomas Doran <tdoran@yelp.com>
2020-07-14 23:48:06 +0100

    Add more movement logic

--- screen/move.go
+++ screen/move.go
@@ -6,6 +6,7 @@ import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 
+	"github.com/bobtfish/mayhem/character"
 	"github.com/bobtfish/mayhem/logical"
 	"github.com/bobtfish/mayhem/render"
 )
@@ -55,7 +56,7 @@ func (screen *MoveFindCharacterScreen) Step(ss pixel.Picture, win *pixelgl.Windo
 		return screen.NextMove()
 	}
 	if win.JustPressed(pixelgl.KeyS) {
-		// FIXME work out what's in this square, start moving it if movable
+		// FIXME work out what's in this square, start moving it if movable and it belongs to the current player
 	}
 
 	return screen
@@ -71,4 +72,53 @@ func (screen *MoveFindCharacterScreen) NextMove() GameScreen {
 		WithBoard: screen.WithBoard,
 		PlayerIdx: screen.PlayerIdx + 1,
 	}
+}
+
+type MoveGroundCharacterScreen struct {
+	*WithBoard
+	PlayerIdx    int
+	Character    *character.Character // FIXME needs to be able to be a player
+	MovementLeft int
+}
+
+func (screen *MoveGroundCharacterScreen) Enter(ss pixel.Picture, win *pixelgl.Window) {
+	ClearScreen(ss, win)
+	fmt.Printf("Enter move ground character screen for player %d\n", screen.PlayerIdx+1)
+}
+
+func (screen *MoveGroundCharacterScreen) Step(ss pixel.Picture, win *pixelgl.Window) GameScreen {
+	batch := screen.WithBoard.DrawBoard(ss, win)
+	screen.WithBoard.DrawCursor(ss, batch)
+	screen.WithBoard.MoveCursor(win)
+	batch.Draw(win)
+
+	direction := captureDirectionKey(win)
+	if direction != logical.ZeroVec() {
+		// FIXME work out if we can move to this square at all - if not ignore, if nothing move to it, if something attack it if owned by other player
+	}
+	return screen
+}
+
+type MoveFlyingCharacterScreen struct {
+	*WithBoard
+	PlayerIdx int
+	Character *character.Character // FIXME needs to be able to be a player
+}
+
+func (screen *MoveFlyingCharacterScreen) Enter(ss pixel.Picture, win *pixelgl.Window) {
+	ClearScreen(ss, win)
+	screen.WithBoard.CursorSprite = CURSOR_FLY
+	fmt.Printf("Enter move flying character screen for player %d\n", screen.PlayerIdx+1)
+}
+
+func (screen *MoveFlyingCharacterScreen) Step(ss pixel.Picture, win *pixelgl.Window) GameScreen {
+	batch := screen.WithBoard.DrawBoard(ss, win)
+	screen.WithBoard.DrawCursor(ss, batch)
+	screen.WithBoard.MoveCursor(win)
+	batch.Draw(win)
+
+	if win.JustPressed(pixelgl.KeyS) {
+		// FIXME work out what's in this square, if nothing move to it, if something attack it
+	}
+	return screen
 }
