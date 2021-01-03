d2f4825f423e2473b51c9a283ccba151c4e5d105
Tomas Doran <tdoran@yelp.com>
2021-01-03 20:53:45 +0000

    Get cast working

--- spellswithscreen/magic_wood.go
+++ spellswithscreen/magic_wood.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"sort"
 
+	"github.com/bobtfish/mayhem/rand"
+
 	"github.com/bobtfish/mayhem/character"
 	"github.com/bobtfish/mayhem/game"
 	"github.com/bobtfish/mayhem/logical"
@@ -19,6 +21,7 @@ type MagicWoodSpellScreen struct {
 	CleanupFunc func() // This is a closure that removes the spell from the player after casting, called when leaving
 	PlayerIdx   int
 	MaybeTiles  []logical.Vec
+	LaidTiles   int
 }
 
 func (screen *MagicWoodSpellScreen) Enter(ctx screeniface.GameCtx) {
@@ -26,7 +29,7 @@ func (screen *MagicWoodSpellScreen) Enter(ctx screeniface.GameCtx) {
 }
 
 func (screen *MagicWoodSpellScreen) Step(ctx screeniface.GameCtx) screeniface.GameScreen {
-	if len(screen.MaybeTiles) == 0 {
+	if len(screen.MaybeTiles) == 0 || screen.LaidTiles == 8 {
 		screen.CleanupFunc()
 		return &screens.Pause{
 			NextScreen: screen.NextScreen,
@@ -45,13 +48,15 @@ func (screen *MagicWoodSpellScreen) Step(ctx screeniface.GameCtx) screeniface.Ga
 	grid.PlaceGameObject(placeAt, &character.Character{
 		Name:            "Magic Wood",
 		Sprite:          magicWoodSpriteVec(),
-		Color:           render.GetColor(255, 255, 255),
-		Defence:         2,
+		Color:           render.GetColor(0, 252, 0),
+		Defence:         2, // FIXME
 		MagicResistance: 5, // FIXME
+		Mountable:       true,
 
 		// FIXME - ugh this is gross - would it be better done up a level?
 		BelongsTo: player,
 	})
+	screen.LaidTiles++
 
 	batch.Draw(win)
 
@@ -68,31 +73,64 @@ func getMagicWoodTiles(ctx screeniface.GameCtx, playerIdx int) []logical.Vec {
 
 	// Find all tiles we could possibly put something on
 	maybeTiles := make([]logical.Vec, 0)
-	for x := -4; x <= 4; x++ {
-		for y := 4; y >= -4; y-- {
+	for x := -8; x <= 8; x++ {
+		for y := 8; y >= -8; y-- {
 			vecAdjust := logical.V(x, y)
 			possibleVec := fromPosition.Add(vecAdjust)
-			fmt.Printf("Consider possible Vec X%d Y%d\n", possibleVec.X, possibleVec.Y)
-			if grid.AsRect().Contains(possibleVec) && !vecAdjust.Equals(logical.ZeroVec()) && grid.GetGameObject(possibleVec).IsEmpty() && adjacentsAreEmpty(ctx, possibleVec) {
-				fmt.Printf("Add possible Vec X%d Y%d\n", possibleVec.X, possibleVec.Y)
+			//fmt.Printf("Consider possible Vec X%d Y%d\n", possibleVec.X, possibleVec.Y)
+			if !grid.AsRect().Contains(possibleVec) {
+				continue
+			}
+			if vecAdjust.Equals(logical.ZeroVec()) {
+				continue
+			}
+			if possibleVec.Distance(fromPosition) > 8 {
+				continue
+			}
+			if !grid.GetGameObject(possibleVec).IsEmpty() {
+				continue
+			}
+			// FIXME - adjacent to the player is OK
+			if adjacentsAreNotMagicWood(ctx, possibleVec) {
+				//fmt.Printf("Add possible Vec X%d Y%d\n", possibleVec.X, possibleVec.Y)
 				maybeTiles = append(maybeTiles, possibleVec)
 			}
 		}
 	}
+
+	// Sort them, nearest to the wizard first
 	d := logical.DistanceSortedVecs{
 		From: fromPosition,
 		List: maybeTiles,
 	}
-	sort.Reverse(d)
-	return d.List
+	sort.Sort(d)
+
+	// Produce a list of the equsl closest possible tiles
+	minDistance := d.List[0].Distance(fromPosition)
+	actualList := make([]logical.Vec, 0)
+	if len(d.List) == 0 {
+		return d.List
+	}
+	for i := 0; i < len(d.List); i++ {
+		if d.List[i].Distance(fromPosition) > minDistance {
+			break
+		}
+		actualList = append(actualList, d.List[i])
+	}
+
+	rand.Shuffle(len(actualList), func(i, j int) { actualList[i], actualList[j] = actualList[j], actualList[i] })
+
+	return actualList
 }
 
-func adjacentsAreEmpty(ctx screeniface.GameCtx, location logical.Vec) bool {
+func adjacentsAreNotMagicWood(ctx screeniface.GameCtx, location logical.Vec) bool {
 	grid := ctx.GetGrid()
 	adjS := grid.AsRect().Adjacents(location)
 	adjEmpty := true
 	for _, adj := range adjS {
-		if !grid.GetGameObject(adj).IsEmpty() {
+		ob := grid.GetGameObject(adj)
+		char, isCharacter := ob.(*character.Character)
+		if isCharacter && char.Name == "Magic Wood" {
 			adjEmpty = false
 			break
 		}
@@ -108,8 +146,9 @@ func init() {
 	spelliface.CreateSpell(ScreenSpell{
 		ASpell: spells.ASpell{
 			Name:          "Magic Wood",
-			CastingChance: 100, // FIXME
-			CastRange:     6,   // FIXME
+			LawRating:     1,
+			CastingChance: 80,
+			CastRange:     0, // Lol this is a hack
 		},
 		TakeOverFunc: func(ctx screeniface.GameCtx, cleanupFunc func(), nextScreen screeniface.GameScreen, playerIdx int, target logical.Vec) screeniface.GameScreen {
 			return &MagicWoodSpellScreen{
@@ -119,4 +158,6 @@ func init() {
 			}
 		},
 	})
+	// Shadow wood - get 8 tiles, must be 1 space between other shadow wood tiles, placed by player\
+	//               chaos 1,casting chance 50%, range 8
 }
